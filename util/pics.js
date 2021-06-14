29655bda8cff0ca4317445889306f683e72c1a52
uhkkzm <uhkkzm@yandex.com>
2021-06-14 21:18:41 +0530

    official pics are first in order

--- util/pics.js
+++ util/pics.js
@@ -3,70 +3,72 @@ const { Collection } = require("discord.js");
 
 const pics = new Collection();
 
-pics.set("cz", ["https://i.imgur.com/PDKa74q.jpg"]);
+pics.set("cz", [
+  "https://i.imgur.com/PDKa74q.jpg", // o
+]);
 pics.set("yc", [
-  "https://i.imgur.com/FtOzzhU.png",
-  "https://i.imgur.com/Teqt0Az.png",
-  "https://i.imgur.com/4Mu6Psy.jpg",
-  "https://i.imgur.com/8gx6oqJ.jpg",
-  "https://i.imgur.com/lwrdfPk.jpg",
-  "https://i.imgur.com/YXg8LLB.png",
-  "https://i.imgur.com/qG0b3DG.jpg",
-  "https://i.imgur.com/RRzaTjv.jpg",
-  "https://i.imgur.com/pMyv07W.jpg",
+  "https://i.imgur.com/RRzaTjv.jpg", // o
+  "https://i.imgur.com/pMyv07W.jpg", // o
+  "https://i.imgur.com/FtOzzhU.png", // o -> xl
+  "https://i.imgur.com/Teqt0Az.png", // m -> xl
+  "https://i.imgur.com/4Mu6Psy.jpg", // m -> jas
+  "https://i.imgur.com/8gx6oqJ.jpg", // m -> xq
+  "https://i.imgur.com/lwrdfPk.jpg", // m
+  "https://i.imgur.com/YXg8LLB.png", // m
+  "https://i.imgur.com/qG0b3DG.jpg", // m
 ]);
 pics.set("js", [
-  "https://i.imgur.com/95mMTHp.jpg",
-  "https://i.imgur.com/2kzBHyZ.jpg",
-  "https://i.imgur.com/4Mu6Psy.jpg",
-  "https://i.imgur.com/Einzxqi.jpg",
-  "https://i.imgur.com/1qPFptr.jpg",
-  "https://i.imgur.com/GfmLr9C.jpg",
-  "https://i.imgur.com/HDfsg0y.jpg",
+  "https://i.imgur.com/GfmLr9C.jpg", // o
+  "https://i.imgur.com/HDfsg0y.jpg", // o
+  "https://i.imgur.com/95mMTHp.jpg", // o
+  "https://i.imgur.com/2kzBHyZ.jpg", // o
+  "https://i.imgur.com/4Mu6Psy.jpg", // m -> yc
+  "https://i.imgur.com/Einzxqi.jpg", // m -> cy
+  "https://i.imgur.com/1qPFptr.jpg", // m
 ]);
 pics.set("mx", [
-  "https://i.imgur.com/1TfcGq4.png",
-  "https://i.imgur.com/GwaYI8E.jpg",
+  "https://i.imgur.com/1TfcGq4.png", // o
+  "https://i.imgur.com/GwaYI8E.jpg", // o
 ]);
 pics.set("cw", [
-  "https://i.imgur.com/9lQjD1i.png",
-  "https://i.imgur.com/7L7pKaS.png",
-  "https://i.imgur.com/SE3DO2W.png",
+  "https://i.imgur.com/9lQjD1i.png", // o
+  "https://i.imgur.com/7L7pKaS.png", // o
+  "https://i.imgur.com/SE3DO2W.png", // o
 ]);
 pics.set("cyc", [
-  "https://i.imgur.com/Xcht5Gn.jpg",
-  "https://i.imgur.com/BCdYBIV.png",
-  "https://i.imgur.com/pmFpQq3.jpg",
+  "https://i.imgur.com/Xcht5Gn.jpg", // m
+  "https://i.imgur.com/BCdYBIV.png", // m
+  "https://i.imgur.com/pmFpQq3.jpg", // m
 ]);
 pics.set("xq", [
-  "https://i.imgur.com/4nVc0If.jpg",
-  "https://i.imgur.com/tT9jNau.jpg",
-  "https://i.imgur.com/8gx6oqJ.jpg",
-  "https://i.imgur.com/RTfyN3V.jpg",
-  "https://i.imgur.com/6B8gvFv.jpg",
-  "https://i.imgur.com/OfIJHlY.jpg",
-  "https://i.imgur.com/Av9JMFf.jpeg",
-  "https://i.imgur.com/iuUCymA.jpeg",
-  "https://i.imgur.com/JIFgl8p.jpeg",
+  "https://i.imgur.com/Av9JMFf.jpeg", // o
+  "https://i.imgur.com/iuUCymA.jpeg", // o
+  "https://i.imgur.com/JIFgl8p.jpeg", // o
+  "https://i.imgur.com/4nVc0If.jpg", // m
+  "https://i.imgur.com/tT9jNau.jpg", // m
+  "https://i.imgur.com/8gx6oqJ.jpg", // m
+  "https://i.imgur.com/RTfyN3V.jpg", // m
+  "https://i.imgur.com/6B8gvFv.jpg", // m
+  "https://i.imgur.com/OfIJHlY.jpg", // m
 ]);
 pics.set("cy", [
-  "https://i.imgur.com/Einzxqi.jpg",
-  "https://i.imgur.com/ZXa73c1.jpg",
-  "https://i.imgur.com/TUM2ibL.jpg",
-  "https://i.imgur.com/nddt64t.jpg",
-  "https://i.imgur.com/lLFQaet.jpg",
-  "https://i.imgur.com/YzOYiRq.jpg",
+  "https://i.imgur.com/Einzxqi.jpg", // m
+  "https://i.imgur.com/ZXa73c1.jpg", // m
+  "https://i.imgur.com/TUM2ibL.jpg", // m
+  "https://i.imgur.com/nddt64t.jpg", // m
+  "https://i.imgur.com/lLFQaet.jpg", // m
+  "https://i.imgur.com/YzOYiRq.jpg", // m
 ]);
 pics.set("xl", [
-  "https://i.imgur.com/FtOzzhU.png",
-  "https://i.imgur.com/Teqt0Az.png",
-  "https://i.imgur.com/3e4g35N.jpg",
+  "https://i.imgur.com/FtOzzhU.png", // o -> yc
+  "https://i.imgur.com/Teqt0Az.png", // m
+  "https://i.imgur.com/3e4g35N.jpg", // m
 ]);
 pics.set("qy", [
-  "https://i.imgur.com/GI3X2uP.png",
-  "https://i.imgur.com/UvjITnK.jpg",
-  "https://i.imgur.com/QoIlyeC.jpg",
-  "https://i.imgur.com/lxRVFCD.jpg",
+  "https://i.imgur.com/GI3X2uP.png", // o
+  "https://i.imgur.com/UvjITnK.jpg", // o
+  "https://i.imgur.com/QoIlyeC.jpg", // o
+  "https://i.imgur.com/lxRVFCD.jpg", // o
 ]);
 
 module.exports = pics;
